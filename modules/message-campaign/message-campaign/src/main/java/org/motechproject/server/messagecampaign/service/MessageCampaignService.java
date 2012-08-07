e17252538607e388a64da30b439f43bcb2532b1a
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-08-07 11:42:48 +0200

    llewczynski | #132 | Moved: 1) functional modules to modules folder (grouped similar modules) 2) platform components to platform folder
    Change-Id: I8864b0dffe2879a35d8d3147fbebc44901451e5d

---
+++ modules/message-campaign/message-campaign/src/main/java/org/motechproject/server/messagecampaign/service/MessageCampaignService.java
@@ -0,0 +1,34 @@
+package org.motechproject.server.messagecampaign.service;
+
+import org.motechproject.server.messagecampaign.contract.CampaignRequest;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * \ingroup MessageCampaign
+ */
+public interface MessageCampaignService {
+    /** Enrolls the external id into the campaign as specified in the request. The enrolled entity will have events raised against it according to the campaign definition.
+     *
+     * @param enrollRequest
+     */
+    void startFor(CampaignRequest enrollRequest);
+    /** Unenrolls an external from the campaign as specified in the request. The entity will no longer receive events from the campaign.
+     *
+     * @param enrollRequest
+     */
+    void stopAll(CampaignRequest enrollRequest);
+
+    /** Searches and returns the Campaign Enrollment Records as per the criteria in the given CampaignEnrollmentsQuery
+     * The query consists of various criteria based on Status, ExternalId and CampaignName of the CampaignEnrollment
+     *
+     * @param query
+     * @return List<CampaignEnrollmentRecord>
+     */
+    List<CampaignEnrollmentRecord> search(CampaignEnrollmentsQuery query);
+
+    // TODO: use DateTime
+    Map<String, List<Date>> getCampaignTimings(String externalId, String campaignName, Date startDate, Date endDate);
+}
